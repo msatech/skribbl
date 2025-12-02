498660439ac282718305726acafa167e438cb456
Deepu <deepuku049@gmail.com>
2025-12-02 17:43:18 +0000

    why onhome screen connecting ...

--- src/app/(game)/room/[roomId]/page.tsx
+++ src/app/(game)/room/[roomId]/page.tsx
@@ -1,16 +1,6 @@
 'use client';
-import { useParams } from 'next/navigation';
 import GameRoom from './game-room';
-import { SocketProvider } from '@/contexts/socket-context';
-
 
 export default function GameRoomPage() {
-  const params = useParams();
-  const roomId = params.roomId as string;
-  
-  return (
-    <SocketProvider roomId={roomId}>
-        <GameRoom />
-    </SocketProvider>
-  );
+  return <GameRoom />;
 }
